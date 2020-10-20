f728bea9281763e3e12e864d7ca115aab64961f5
yisusc99 <72536324+yisusc99@users.noreply.github.com>
2020-10-19 17:53:58 -0700

    Add files via upload

---
+++ Librerias/Librerias/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librerias
+{
+    public class Operaciones
+    {
+        public static double suma(double numero1, double numero2) {
+            return numero1 + numero2;
+        }
+        public static double resta(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
+        public static double division(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+        public static double multiplicacion(double numero1, double numero2)
+        {
+            return numero1 / numero2;
+        }
+    }
+}
